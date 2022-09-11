3871bec5849c26414bbeca17c55dede4f212e9ec
Heitor da Costa <heitor2000costa@gmail.com>
2022-09-11 15:07:01 -0300

    página de projetos atualizada

--- portfolio-web/src/paginas/Projetos.js
+++ portfolio-web/src/paginas/Projetos.js
@@ -3,12 +3,14 @@ import Header from "../componentes/Header";
 import FrontendImage from '../imagens/frontend-online-store-image.png';
 import TryunfoImage from '../imagens/tryunfo-image.png';
 import CardProjetos from "./cardProjetos";
+import TrybeWalletImage from '../imagens/imagem-trybewallet.png';
 
 import './Projetos.css';
 
 export default function Projetos() {
   const projetos = [
-    { url: 'https://frontend-online-store-hektho.vercel.app/', 
+    { 
+      url: 'https://frontend-online-store-hektho.vercel.app/', 
       name: 'Frontend Online Store', 
       img: FrontendImage,
       detalhes: {
@@ -16,16 +18,28 @@ export default function Projetos() {
         github: 'https://github.com/hektho/Frontend-Online-Store',
       }
     },
-    { url: 'https://tryunfo-hektho.vercel.app/', 
+    { 
+      url: 'https://tryunfo-hektho.vercel.app/', 
       name: 'Tryunfo', 
       img: TryunfoImage, 
       detalhes: {
         descricao: 'Projeto que consiste na criação de cartas do famoso jogo "Super Trunfo" e salvá-las no baralho.',
         github: 'https://github.com/hektho/Tryunfo',
-    }}
-  ]
+      } 
+    },
+    {
+      url: 'https://trybe-wallet-hektho.vercel.app/',
+      name: 'TrybeWallet',
+      img: TrybeWalletImage,
+      detalhes: {
+        descricao: 'Projeto que simula uma carteira de controle de gastos com conversor de moedas.',
+        github: 'https://github.com/hektho/TrybeWallet'
+      }
+    },
+  ];
+
   return (
-    <div>
+    <div className="div-main-container-projetos">
       <Header />
       <section className="main-projetos">
         {projetos.map(({ url, name, img, detalhes }) => (
